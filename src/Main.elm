ed1818d1fdabe789d9ad8482af74088efaa5cb51
maron8676 <maron8676@gmail.com>
2019-09-14 01:24:51 +0900

    neat-layoutを導入した

--- src/Main.elm
+++ src/Main.elm
@@ -2,7 +2,12 @@ module Main exposing (main)
 
 import Browser
 import Html exposing (..)
+import Html.Attributes exposing (..)
 import Html.Events exposing (..)
+import Mixin
+import Neat
+import Neat.Layout
+import Neat.Layout.Row exposing (defaultRow)
 import Round
 import Task
 import Time
@@ -15,7 +20,7 @@ import Time
 main =
     Browser.element
         { init = init
-        , view = view
+        , view = neatView
         , update = update
         , subscriptions = subscriptions
         }
@@ -148,10 +153,10 @@ view model =
     div [] <|
         List.append
             [ viewElapseTime (Time.posixToMillis model.now) model.timer
-            , button [ onClick TimerStart ] [ text "start" ]
-            , button [ onClick TimerEnd ] [ text "end" ]
-            , button [ onClick TimerReset ] [ text "reset" ]
-            , h3 [] [ text "History" ]
+            , button [ class "button", class "is-success", onClick TimerStart ] [ text "start" ]
+            , button [ class "button", onClick TimerEnd ] [ text "end" ]
+            , button [ class "button", class "is-warning", onClick TimerReset ] [ text "reset" ]
+            , div [ class "title" ] [ text "History" ]
             ]
         <|
             List.map
@@ -188,3 +193,76 @@ formatTerm time =
             Round.floor 0 <| toFloat (modBy 1000 (time + 50)) / 100
     in
     String.join ":" [ hours, minutes, seconds ] ++ "." ++ milliseconds
+
+
+type MyPadding
+    = MyPadding
+
+
+myPadding : Neat.IsPadding MyPadding
+myPadding =
+    Neat.IsPadding
+        { rem = 0.6
+        }
+
+
+neatView : Model -> Html Msg
+neatView model =
+    div [] <|
+        Neat.toPage <|
+            Neat.setBoundary myPadding <|
+                Neat.Layout.column <|
+                    [ Neat.Layout.rowWith
+                        { defaultRow
+                            | horizontal = Neat.Layout.Row.HCenter
+                        }
+                        [ Neat.fromNoPadding myPadding <|
+                            neatViewElapseTime (Time.posixToMillis model.now) model.timer
+                        ]
+                    , Neat.Layout.row <|
+                        [ Neat.fromNoPadding myPadding <|
+                            Neat.lift button
+                                [ Mixin.class "button"
+                                , Mixin.class "is-success"
+                                , Mixin.fromAttribute <| style "width" "100px"
+                                , Mixin.fromAttribute <| onClick TimerStart
+                                ]
+                                [ Neat.text "start" ]
+                        , Neat.fromNoPadding myPadding <|
+                            Neat.lift button
+                                [ Mixin.class "button"
+                                , Mixin.fromAttribute <| style "width" "100px"
+                                , Mixin.fromAttribute <| onClick TimerEnd
+                                ]
+                                [ Neat.text "end" ]
+                        , Neat.fromNoPadding myPadding <|
+                            Neat.lift button
+                                [ Mixin.class "button"
+                                , Mixin.class "is-warning"
+                                , Mixin.fromAttribute <| style "width" "100px"
+                                , Mixin.fromAttribute <| onClick TimerReset
+                                ]
+                                [ Neat.text "reset" ]
+                        ]
+                    , Neat.Layout.row <|
+                        [ Neat.fromNoPadding myPadding <|
+                            Neat.div [ Mixin.class "title" ] [ Neat.text "History" ]
+                        ]
+                    , Neat.Layout.row <|
+                        List.map
+                            (\x -> Neat.fromNoPadding myPadding <| Neat.div [] [ Neat.text <| formatTerm x ])
+                            model.termHistory
+                    ]
+
+
+neatViewElapseTime : Int -> Timer -> Neat.View Neat.NoPadding Msg
+neatViewElapseTime now timer =
+    case timer of
+        Initial ->
+            Neat.div [] [ Neat.text "0:0:0.0" ]
+
+        Started splitTime started ->
+            Neat.div [] [ Neat.text <| formatTerm <| (splitTime + now - started) ]
+
+        Stop splitTime started ended ->
+            Neat.div [] [ Neat.text <| formatTerm <| (splitTime + ended - started) ]
